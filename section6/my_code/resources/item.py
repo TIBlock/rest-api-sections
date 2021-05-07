402e55976115a7d6ec9103ae130b824b34f64cca
TIBlock <taylorblocker@gmail.com>
2021-05-07 16:13:04 -0500

    committing SQLAlchemy changes

--- section6/my_code/resources/item.py
+++ section6/my_code/resources/item.py
@@ -61,17 +61,5 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-    TABLE_NAME = 'items'
-
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM {table}".format(table=self.TABLE_NAME)
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-        connection.close()
-
-        return {'items': items}
+        return {'items': [item.json() for item in ItemModel.query.all()]}
