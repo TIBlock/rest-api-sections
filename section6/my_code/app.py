bba72148ccdb9ca949baabd1b40fca979c3abd30
TIBlock <taylorblocker@gmail.com>
2021-05-12 13:37:29 -0500

    completed updates to flask-jwt-extended and token refreshing etc

--- section6/my_code/app.py
+++ section6/my_code/app.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from blocklist import BLOCKLIST
 
-from resources.user import UserRegister, User, UserLogin, TokenRefresh
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -31,17 +31,17 @@ def add_claims_to_jwt(identity):
     
 @jwt.token_in_blocklist_loader
 def check_if_token_in_blocklist(self, decrypted_token):
-    return decrypted_token['sub'] in BLOCKLIST
+    return decrypted_token['jti'] in BLOCKLIST
 
 @jwt.expired_token_loader
-def expired_token_callback():
+def expired_token_callback(self, error):
     return jsonify({
         'description': 'The token has expired.',
         'error': 'token_expired'
     }), 401
 
 @jwt.invalid_token_loader
-def invalid_token_callback(self, error):
+def invalid_token_callback(error):
     return jsonify({
         'desription': 'Signature verification failed.',
         'error': 'invalid_token'
@@ -80,6 +80,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
 api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
--- section6/my_code/resources/user.py
+++ section6/my_code/resources/user.py
@@ -1,14 +1,15 @@
-
-from flask_jwt_extended.utils import get_jwt_identity
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import (
     create_access_token, 
     create_refresh_token, 
     jwt_required, 
-    get_jwt_identity
+    get_jwt_identity,
+    get_jwt
 )
+
 from models.user import UserModel
+from blocklist import BLOCKLIST
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username',
@@ -71,6 +72,13 @@ class UserLogin(Resource):
 
         #create access token
         #create refresh token
+
+class UserLogout(Resource):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()['jti']
+        BLOCKLIST.add(jti)
+        return {'message': 'successfully logged out'}, 200
 
 
 class TokenRefresh(Resource):
